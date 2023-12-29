dee909f95c9fb27662ed94293dfe67c8f38bfac7
yapsbjj <ya.ramdane@laposte.net>
2023-12-29 14:12:41 +0100

    add 404 error

---
+++ src/components/views/Modale.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bloc-modale">
+    <div class="overlay"></div>
+
+    <div class="modale-card">
+        <div class="btn-modale">x</div>
+        <h2>Le contenu de la modale</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Modale'
+
+}
+</script>
+
+<style>
+
+</style>
